29db5d5e6550481668f52e6ab93a4c836c502c29
Hac Hai Pham <hpham.ux@gmail.com>
2021-10-21 16:53:48 +0200

    Run a test on api call

--- crypto-alerts-final-project/src/App.test.js
+++ crypto-alerts-final-project/src/App.test.js
@@ -4,10 +4,22 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 import { App } from './App.js';
+import { GetData } from './components/utils/GetCoinData';
 
-test('Does App component exist?', () => {
-    console.log(App);
-        expect(App).toEqual(true);
+test('Is code 200', async () => {
+
+    //make test async so that expect will wait for data to be received
+    const data = async () => {
+        
+        const code = await GetData.ping();
+        console.log(code);
+        return code;
+
+            
+    }
+    const result = await data();
+    expect(result.code).toEqual(200);
+       
   });
     
   
--- crypto-alerts-final-project/src/components/utils/GetCoinData.js
+++ crypto-alerts-final-project/src/components/utils/GetCoinData.js
@@ -4,6 +4,12 @@ const GetData = (() => {
    
     const CoinGeckoClient = new CoinGecko();
 
+    const getCoinsList = async () => {
+        let coinsList = await CoinGeckoClient.coins.list();
+        return coinsList;
+    };
+  
+
     const ping = async() => {
         const data = await CoinGeckoClient.ping();
         return data;
